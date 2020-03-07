c0896735c4becd39d487d9e72d101a639cf0a023
marshall <null>
2020-03-06 19:44:57 -0700

    change to blogrollmd

--- content/blogroll.md
+++ content/blogroll.md
@@ -4,3 +4,6 @@ date: 2020-02-25T20:01:24-07:00
 lastmod: 2020-02-25T20:20:24-07:00
 draft: false
 ---
+
+Recent reading list:
+
--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ set -e
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
 # Build the project.
-hugo -t tale
+hugo
 
 # Go To Public folder
 cd public
@@ -19,6 +19,7 @@ msg="rebuilding site $(date)"
 if [ -n "$*" ]; then
 	msg="$*"
 fi
+
 git commit -m "$msg"
 
 # Push source and build repos.
@@ -34,6 +35,7 @@ msg="rebuilding site $(date)"
 if [ -n "$*" ]; then
 	msg="$*"
 fi
+
 git commit -m "$msg"
 
 # Push source and build repos.
